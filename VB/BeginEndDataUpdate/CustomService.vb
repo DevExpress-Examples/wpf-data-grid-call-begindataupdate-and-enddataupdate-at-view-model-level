27fa1b993355100e42770580bd0d1b607a776d83
DevExpressExampleBot <fakeEmail>
2021-08-18 18:32:04 +0300

    Source auto update [skip ci]

---
+++ VB/BeginEndDataUpdate/CustomService.vb
@@ -0,0 +1,42 @@
+﻿Imports DevExpress.Mvvm.UI
+Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+
+Namespace BeginEndDataUpdate
+
+	Public Interface ICustomService
+		Sub BeginUpdate()
+		Sub EndUpdate()
+	End Interface
+
+	Friend Class CustomService
+		Inherits ServiceBase
+		Implements ICustomService
+
+		Private ReadOnly Property ActuaGridControl() As GridControl
+			Get
+				Return TryCast(AssociatedObject, GridControl)
+			End Get
+		End Property
+
+		Protected Overrides Sub OnAttached()
+			If ActuaGridControl Is Nothing Then
+				Throw New InvalidOperationException("This service can be attached only to the GridControl.")
+			End If
+			MyBase.OnAttached()
+		End Sub
+
+		Public Sub BeginUpdate() Implements ICustomService.BeginUpdate
+			ActuaGridControl.BeginDataUpdate()
+		End Sub
+
+		Public Sub EndUpdate() Implements ICustomService.EndUpdate
+			 ActuaGridControl.EndDataUpdate()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/BeginEndDataUpdate/DataItem.vb
@@ -0,0 +1,35 @@
+﻿Imports DevExpress.Mvvm
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace BeginEndDataUpdate
+	Public Class DataItem
+		Inherits BindableBase
+
+		Public Property Name() As String
+			Get
+				Return GetValue(Of String)(NameOf(Name))
+			End Get
+			Set(ByVal value As String)
+				SetValue(value)
+			End Set
+		End Property
+
+		Public Property Value() As Integer
+			Get
+				Return GetValue(Of Integer)(NameOf(Value))
+			End Get
+			Set(ByVal value As Integer)
+				SetValue(value)
+			End Set
+		End Property
+
+		Public Overrides Function ToString() As String
+			Return Name
+		End Function
+	End Class
+End Namespace
